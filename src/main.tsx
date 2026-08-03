4abf1d0b47b0b08298011fc5b1611e1489c7f249
rdp12356 <rdp12356@outlook.com>
2026-08-03 20:58:13 +0530

    chore: add ErrorBoundary and vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 import App from "./App.tsx";
 
@@ -25,6 +26,8 @@ if ("serviceWorker" in navigator && import.meta.env.PROD) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
